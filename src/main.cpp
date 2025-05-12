8ed1b0b92478272a92b67367178037af79d28e9d
muskyAzure <tanakit.j@ku.th>
2025-05-12 18:00:22 +0700

    add git ignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-#include "tgaimage.h"
+#include "../include/tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-
-
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
